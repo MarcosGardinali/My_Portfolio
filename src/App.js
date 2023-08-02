b0ed5d660830c7d59fab9d8cedf8bbf2cf2f0000
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-08-02 09:48:55 -0300

    .

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import PaginaInicial from "./Paginas";
-
-function App() {
-  return (
-    <PaginaInicial />
-  );
-}
-
-export default App;
--- src/Paginas/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function PaginaInicial() {
-  return (
-    <div>PaginaInicial</div>
-  )
-}
